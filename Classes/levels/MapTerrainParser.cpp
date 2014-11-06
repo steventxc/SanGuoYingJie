2418636b77677402d17816b42868c61738a7b867
Steven.Tian <steventxc@gmail.com>
2014-11-06 16:49:42 +0800

    change data struct for get map terrain info

--- Classes/levels/MapTerrainParser.cpp
+++ Classes/levels/MapTerrainParser.cpp
@@ -12,6 +12,23 @@ USING_NS_CC;
 using std::string;
 
 
+static void split(const std::string& s, const std::string& delim, std::vector< std::string >& ret)
+{
+    size_t last = 0;
+    size_t index = s.find_first_of(delim,last);
+    while (index!=std::string::npos)
+    {
+        ret.push_back(s.substr(last,index-last));
+        last=index+1;
+        index=s.find_first_of(delim,last);
+    }
+    if (index-last>0)
+    {
+        ret.push_back(s.substr(last,index-last));
+    }
+}
+
+
 MapTerrainParser * MapTerrainParser:: create(const std::string& tmxFile)
 {
     MapTerrainParser *ret = new MapTerrainParser();
@@ -47,7 +64,7 @@ bool MapTerrainParser:: parseXMLFile(const string &xmlFilename)
 void MapTerrainParser:: startElement(void *ctx, const char *name, const char **atts)
 {
     CC_UNUSED_PARAM(ctx);
-//    TMXMapInfo *tmxMapInfo = this;
+    
     string elementName(name);
     ValueMap attributeDict;
     if (atts && atts[0])
@@ -60,43 +77,48 @@ void MapTerrainParser:: startElement(void *ctx, const char *name, const char **a
         }
     }
     
-    if (elementName == "terraintypes") {
+    if (elementName == "map") {
+        
+        _layerTerrainInfo.clear();
+        
+    } else if (elementName == "tileset") {
+        
+        // create a new LayerTerrainInfo object.
+        if (_terraininfo != nullptr)
+            CC_SAFE_RELEASE_NULL(_terraininfo);
+        
+        
+        _terraininfo = LayerTerrainInfo::create();
+        _terraininfo->setName(attributeDict["name"].asString());
+        
+        
+    } else if (elementName == "terraintypes") {
         
         // <terraintypes> tag start
-        terrainTypes.clear();
+        _terraintypes.clear();
         
     } else if (elementName == "terrain") {
-        // get terrain types in order
-        CCLOG("terrain type name: %s", attributeDict["name"].asString().c_str());
-        terrainTypes.push_back(attributeDict["name"]);
+        // get terrain types of current tile set in order
+        _terraintypes.push_back(attributeDict["name"]);
         
     } else if (elementName == "tile") {
+ 
+        std::vector<string> data;
+        split(attributeDict["terrain"].asString(), ",", data);
+        Value value;
         
-        for (decltype(attributeDict.begin())it = attributeDict.begin(); it!=attributeDict.end(); ++it) {
-            
-            CCLOG("key: %s, value: %s", it->first.c_str(), it->second.getDescription().c_str());
+        // entire tile have only one terrain type
+        for (int i =0; i<data.size(); i++)
+        {
+            if (!data[i].empty()) {
+                value = data[i];
+                break;
+            }
         }
         
-//        if (tmxMapInfo->getParentElement() == TMXPropertyLayer)
-//        {
-//            TMXLayerInfo* layer = tmxMapInfo->getLayers().back();
-//            Size layerSize = layer->_layerSize;
-//            uint32_t gid = static_cast<uint32_t>(attributeDict["gid"].asInt());
-//            int tilesAmount = layerSize.width*layerSize.height;
-//            
-//            if (_xmlTileIndex < tilesAmount)
-//            {
-//                layer->_tiles[_xmlTileIndex++] = gid;
-//            }
-//        }
-//        else
-//        {
-//            TMXTilesetInfo* info = tmxMapInfo->getTilesets().back();
-//            tmxMapInfo->setParentGID(info->_firstGid + attributeDict["id"].asInt());
-//            tmxMapInfo->getTileProperties()[tmxMapInfo->getParentGID()] = Value(ValueMap());
-//            tmxMapInfo->setParentElement(TMXPropertyTile);
-//        }
-        
+        auto tileTerrain = LayerTerrainInfo::TileTerrain::create(attributeDict["id"].asInt(), value);
+        //
+        _tileTerrainInfo.pushBack(tileTerrain);
     }
 }
 
@@ -106,42 +128,33 @@ void MapTerrainParser:: endElement(void *ctx, const char *name)
     CC_UNUSED_PARAM(ctx);
     string elementName(name);
     
-    if (elementName == "tile") {
-        CCLOG("============================");
+    if (elementName == "tileset") {
+        
+        // add terrain info for every specified tile.
+        _terraininfo->setTileTerrainInfo(_tileTerrainInfo);
+        
+        // add a layer terrain infomation to vector and release the temporary one for reuse.
+        _layerTerrainInfo.pushBack(_terraininfo);
+        CC_SAFE_RELEASE_NULL(_terraininfo);
+        
+    } else if (elementName == "terraintypes") {
+        
+        // end of </terraintypes>, set the terrain types info.
+        if (nullptr != _terraininfo) {
+            _terraininfo->setTerrainTypes(_terraintypes);
+        }
+        
+        // prepare to add tiles.
+        _tileTerrainInfo.clear();
+        
     }
+
 }
 
 void MapTerrainParser:: textHandler(void *ctx, const char *ch, int len)
 {
     CC_UNUSED_PARAM(ctx);
-//    TMXMapInfo *tmxMapInfo = this;
-    string elementName(ch);
-    
-    CCLOG("********** %s", elementName.c_str());
-    
-//    if (tmxMapInfo->isStoringCharacters())
-//    {
-//        std::string currentString = tmxMapInfo->getCurrentString();
-//        currentString += text;
-//        tmxMapInfo->setCurrentString(currentString.c_str());
-//    }
-
-}
-
-void split(std::string& s, std::string& delim,std::vector< std::string >* ret)
-{
-    size_t last = 0;
-    size_t index=s.find_first_of(delim,last);
-    while (index!=std::string::npos)
-    {
-        ret->push_back(s.substr(last,index-last));
-        last=index+1;
-        index=s.find_first_of(delim,last);
-    }
-    if (index-last>0)
-    {
-        ret->push_back(s.substr(last,index-last));
-    }
+    string text(ch, len);
 }
 
 
--- Classes/levels/MapTerrainParser.h
+++ Classes/levels/MapTerrainParser.h
@@ -10,19 +10,54 @@
 #define __SanGuoYingJie__MapTerrainParser__
 
 #include "cocos2d.h"
+#include <unordered_map>
 
-class TileTerrain;
+class LayerTerrainInfo : public cocos2d::Ref
+{
+public:
+    
+    class TileTerrain : public cocos2d::Ref
+    {
+    public:
+        int _id;
+        cocos2d::Value _value;
+        
+        unsigned terrainType;
+        
+        static TileTerrain* create(int id, cocos2d::Value value)
+        {
+            TileTerrain *ret = new TileTerrain(id, value);
+            ret->autorelease();
+            return ret;
+        }
+        
+    protected:
+        TileTerrain(int id, cocos2d::Value value): _id(id), _value(value) {};
+    };
+    
+public:
+    CREATE_FUNC(LayerTerrainInfo);
+    
+    /*
+     *  tileSetName same as tile set which the terrain information belonged to.
+     */
+    CC_SYNTHESIZE_PASS_BY_REF(std::string, _tileSetName, Name);
+    
+    CC_SYNTHESIZE_PASS_BY_REF(cocos2d::ValueVector, _terrainTypes, TerrainTypes);
+    
+    CC_SYNTHESIZE_PASS_BY_REF(cocos2d::Vector<TileTerrain*>, _tileTerrainInfo, TileTerrainInfo);
+    
+protected:
+    bool init(){ return true;}
+    
+};
 
-class MapTerrainParser : public cocos2d::Ref, public cocos2d::SAXDelegator {
+
+class MapTerrainParser : public cocos2d::Ref, public cocos2d::SAXDelegator
+{
 public:
     /** creates a TMX Format with a tmx file */
     static MapTerrainParser * create(const std::string& tmxFile);
-    
-    /** initializes a TMX format with a  tmx file */
-    bool initWithFile(const std::string& tmxFile);
-    
-    /** initializes parsing of an XML file, either a tmx (Map) file or tsx (Tileset) file */
-    bool parseXMLFile(const std::string& xmlFilename);
     
 public:
     // implement pure virtual methods of SAXDelegator
@@ -31,25 +66,26 @@ public:
     void textHandler(void *ctx, const char *ch, int len);
     
 protected:
-    cocos2d::ValueVector terrainTypes;
-    cocos2d::Vector<TileTerrain*> tileTerrainInfo;
+    /** initializes a TMX format with a  tmx file */
+    bool initWithFile(const std::string& tmxFile);
+    
+    /** initializes parsing of an XML file, either a tmx (Map) file or tsx (Tileset) file */
+    bool parseXMLFile(const std::string& xmlFilename);
+    
+    
+    CC_SYNTHESIZE_READONLY_PASS_BY_REF(cocos2d::Vector<LayerTerrainInfo *>, _layerTerrainInfo, TerrainInfo);
+    
+private:
+    
+    MapTerrainParser() : _terraininfo(nullptr)  {}
+   
+    
+    LayerTerrainInfo *_terraininfo;
+    cocos2d::ValueVector _terraintypes;
+    cocos2d::Vector<LayerTerrainInfo::TileTerrain*> _tileTerrainInfo;
 };
 
 
-class TileTerrain : public cocos2d::Ref
-{
-public:
-    int id;
-    cocos2d::Value value;
-    
-    CREATE_FUNC(TileTerrain);
-    
-    bool init()
-    {
-        id = -1;
-        value = cocos2d::Value::Null;
-        return true;
-    }
-};
+
 
 #endif /* defined(__SanGuoYingJie__MapTerrainParser__) */
