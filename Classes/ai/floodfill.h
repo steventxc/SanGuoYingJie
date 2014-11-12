4917781dd8d2b5082bea3f601f5422d208246de8
StevenTian <steventxc@gmail.com>
2014-11-12 22:20:51 +0800

    implement the flood-fill algorithm

--- Classes/ai/floodfill.h
+++ Classes/ai/floodfill.h
@@ -9,48 +9,143 @@
 #ifndef __SanGuoYingJie__floodfill__
 #define __SanGuoYingJie__floodfill__
 
-#include <stdio.h>
+#include <vector>
+
+using namespace std;
 
 template <class UserState> class FloodFillSearch
 {
-    UserState m_UserStart;
-    unsigned m_Limited;
+public:
+    
+    class Node
+    {
+    public:
+        
+        bool isVisited;
+        
+        Node() : isVisited( false ) { }
+        
+        UserState m_UserState;
+    };
+    
     
 public:
     
     void SetStartStates(UserState &Start, unsigned limited)
     {
-        m_UserStart = Start;
+        m_Start = new Node;
+        m_Start->m_UserState = Start;
+        
         m_Limited = limited;
+        
     }
     
-    void SearchStep(float x, float y)
+    void startSearch()
     {
+        SearchStep(m_Start);
+    }
+    
+    vector<UserState> getResultSet()
+    {
+        vector<UserState> result;
         
-        if (m_UserStart.isValid(x - 1, y) &&
-            ( m_UserStart.GoalDistanceEstimate(m_UserStart) <= m_Limited )
-            ) {
-            SearchStep(x-1, y);
+        typename vector<Node *>::iterator iter;
+        for (iter = m_ValidList.begin(); iter != m_ValidList.end(); iter ++) {
+            result.push_back( (*iter)->m_UserState );
         }
         
-        if (m_UserStart.isValid(x, y - 1) &&
-            ( m_UserStart.GoalDistanceEstimate(m_UserStart) <= m_Limited )
-            ) {
-            SearchStep(x, y-1);
+        return result;
+    }
+    
+    
+    Node * createNode( UserState &State )
+    {
+        Node *node = new Node();
+        
+        if( node )
+        {
+            node->m_UserState = State;
+            return node;
         }
         
-        if (m_UserStart.isValid(x + 1, y) &&
-            ( m_UserStart.GoalDistanceEstimate(m_UserStart) <= m_Limited )
+        delete node;
+        
+        return nullptr;
+    }
+    
+    void destroyNode(Node *node)
+    {
+        delete node;
+    }
+    
+protected:
+    
+    void SearchStep(Node* node)
+    {
+        //        if (! node->m_UserState.IsSameState(m_Start->m_UserState)) {
+        node->isVisited = true;
+        m_ValidList.push_back( node );
+        //        }
+        
+        const int x = node->m_UserState.x;
+        const int y = node->m_UserState.y;
+        
+        if (!node->isVisited &&
+            node->m_UserState.isValid() &&
+            ( node->m_UserState.GoalDistanceEstimate(m_Start->m_UserState) <= m_Limited )
             ) {
-            SearchStep(x+1, y);
+            
+            auto n = node->m_UserState.getNode(this, x-1, y);
+            if (n) {
+                SearchStep(n);
+            }
+            
         }
         
-        if (m_UserStart.isValid(x, y + 1) &&
-            ( m_UserStart.GoalDistanceEstimate(m_UserStart) <= m_Limited )
+        if (!node->isVisited &&
+            node->m_UserState.isValid() &&
+            ( node->m_UserState.GoalDistanceEstimate(m_Start->m_UserState) <= m_Limited )
             ) {
-            SearchStep(x, y+1);
+            
+            auto n = node->m_UserState.getNode(this, x, y-1);
+            if (n) {
+                SearchStep(n);
+            }
+            
+        }
+        
+        if (!node->isVisited &&
+            node->m_UserState.isValid() &&
+            ( node->m_UserState.GoalDistanceEstimate(m_Start->m_UserState) <= m_Limited )
+            ) {
+            
+            auto n = node->m_UserState.getNode(this, x+1, y);
+            if (n) {
+                SearchStep(n);
+            }
+            
+        }
+        
+        if (!node->isVisited &&
+            node->m_UserState.isValid() &&
+            ( node->m_UserState.GoalDistanceEstimate(m_Start->m_UserState) <= m_Limited )
+            ) {
+            
+            auto n = node->m_UserState.getNode(this, x, y+1);
+            if (n) {
+                SearchStep(n);
+            }
+            
         }
     }
+    
+    
+private:
+    
+    Node *m_Start;
+    unsigned m_Limited;
+    vector<Node *> m_ValidList;
+    
 };
 
 #endif /* defined(__SanGuoYingJie__floodfill__) */
