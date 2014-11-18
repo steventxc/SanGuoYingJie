7c694f3aaa741da94a8ca0fc7072171fce8e700b
StevenTian <steventxc@gmail.com>
2014-11-18 21:32:44 +0800

    set terrain info of each troops type

--- Classes/data/TroopsInfo.cpp
+++ Classes/data/TroopsInfo.cpp
@@ -15,18 +15,18 @@ unsigned TroopsInfo:: getTerrainsWithType(TroopsType type)
 {
     switch (type) {
         case TroopsType::INFANTRY :
-            // 0x00011111;
+            // 00011111;
             return MapTerrain::Terrain::PLAIN |  MapTerrain::Terrain::GRASSLAND
             | MapTerrain::Terrain::MOUNTAINLAND | MapTerrain::Terrain::BUILDING
             | MapTerrain::Terrain::FOREST;
             
         case TroopsType::CAVALRY :
-            // 0x00011011
+            // 00011011
             return MapTerrain::Terrain::PLAIN |  MapTerrain::Terrain::GRASSLAND
             | MapTerrain::Terrain::MOUNTAINLAND | MapTerrain::Terrain::BUILDING;
             
         case TroopsType::ARCHER :
-            // 0x00011111;
+            // 00011111;
             return MapTerrain::Terrain::PLAIN |  MapTerrain::Terrain::GRASSLAND
             | MapTerrain::Terrain::MOUNTAINLAND | MapTerrain::Terrain::BUILDING
             | MapTerrain::Terrain::FOREST;
